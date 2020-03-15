4b89bfb7259bc5b4cc7154c9c735f6b8f390ca4f
Kasumi Gunasekara <kasumis@hsenidmobile.com>
2020-03-15 09:41:09 +0530

    Added multiple Socket clients handling

--- src/main/java/hms/dev/knock/KnockKnockClient.java
+++ src/main/java/hms/dev/knock/KnockKnockClient.java
@@ -8,31 +8,37 @@ import java.net.*;
 public class KnockKnockClient {
     public static void main(String[] args) {
 
+        final int NUMBER_OF_CLIENTS = 5;
+
+        for (int i = 0; i < NUMBER_OF_CLIENTS; i++) {
+            final int clientId = i;
+            Thread clientThread = new Thread(() -> initiateClientSession(clientId));
+            clientThread.setName("Client: " + clientId);
+            clientThread.setDaemon(false);
+            clientThread.start();
+        }
+    }
+
+    private static void initiateClientSession(int clientId) {
         String hostName = "localhost";
         int portNumber = 8080;
 
+        LogUtil.logMessage("Initiated client session for: " + clientId);
         try (
                 Socket kkSocket = new Socket(hostName, portNumber);
                 PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
                 BufferedReader in = new BufferedReader(
                         new InputStreamReader(kkSocket.getInputStream()))
         ) {
-            BufferedReader stdIn =
-                    new BufferedReader(new InputStreamReader(System.in));
             String fromServer;
             String fromUser;
 
-            while ((fromServer = in.readLine()) != null) {
-                LogUtil.logMessage("Server: " + fromServer);
-                if (fromServer.equals("BYE."))
-                    break;
+            fromUser = "Testing client[" + clientId + "]";
+            LogUtil.logMessage(fromUser);
+            out.println(fromUser);
+            fromServer = in.readLine();
+            LogUtil.logMessage("Server for client[" + clientId + "]: " + fromServer);
 
-                fromUser = stdIn.readLine();
-                if (fromUser != null) {
-                    LogUtil.logMessage("Client: " + fromUser);
-                    out.println(fromUser);
-                }
-            }
         } catch (UnknownHostException e) {
             LogUtil.logMessage("Don't know about host " + hostName);
             System.exit(1);
--- src/main/java/hms/dev/knock/KnockKnockServer.java
+++ src/main/java/hms/dev/knock/KnockKnockServer.java
@@ -6,38 +6,35 @@ import java.net.*;
 import java.io.*;
 
 public class KnockKnockServer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         int portNumber = 8080;
+        ServerSocket serverSocket = new ServerSocket(portNumber);
 
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            acceptClientConnection(clientSocket);
+        }
+    }
+
+    private static void acceptClientConnection(Socket clientSocket) {
         try (
-                ServerSocket serverSocket = new ServerSocket(portNumber);
-                Socket clientSocket = serverSocket.accept();
                 PrintWriter out =
                         new PrintWriter(clientSocket.getOutputStream(), true);
                 BufferedReader in = new BufferedReader(
                         new InputStreamReader(clientSocket.getInputStream()))
         ) {
+            String inputLine = in.readLine();
+            String outputLine;
 
-            String inputLine, outputLine;
-
-            // Initiate conversation with client
-            KnockKnockProtocol kkp = new KnockKnockProtocol();
-            outputLine = kkp.processInput(null);
-            out.println(outputLine);
-            LogUtil.logMessage("A client connected");
-
-            while ((inputLine = in.readLine()) != null) {
+            if (inputLine != null) {
                 LogUtil.logMessage("Client: " + inputLine);
-                outputLine = kkp.processInput(inputLine);
+                outputLine = inputLine.toUpperCase();
                 out.println(outputLine);
                 LogUtil.logMessage("Server:" + outputLine);
-                if (outputLine.equals("Bye."))
-                    break;
             }
         } catch (IOException e) {
-            System.out.println("Exception caught when trying to listen on port "
-                    + portNumber + " or listening for a connection");
+            System.out.println("Exception caught when trying to listen or listening for a connection");
             System.out.println(e.getMessage());
         }
     }
